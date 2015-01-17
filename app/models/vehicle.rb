b142473394eb19b08561dcb5c7ea72799ac697ff
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-17 16:22:53 -0600

    Fixed an incorrect max length validation on Vehicle.vin

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle < ActiveRecord::Base
   has_many :maintenance_items
 
   validates :title, presence: true
-  validates :vin, length: { max: 17 }
+  validates :vin, length: { maximum: 17 }
   validates :year, numericality: { only_integer: true }, length: { is: 4 }, presence: true
   validates :make, presence: true
   validates :model, presence: true
