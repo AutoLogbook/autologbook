07f362723c6dbe65aab3324a57d349eea894e3ab
Tyler Willingham <tylerwillingham@gmail.com>
2015-01-28 22:06:24 -0600

    Expanding on the MaintenanceService PORO

--- app/services/maintenance_service.rb
+++ app/services/maintenance_service.rb
@@ -6,4 +6,12 @@ class MaintenanceService
   def get_all
     MaintenanceItem.where(vehicle: @vehicle)
   end
+
+  def get_all_by_type(type)
+    MaintenanceItem.where vehicle: @vehicle, maintenance_type_id: type.id
+  end
+
+  def get_type_by_slug(slug)
+    MaintenanceType.find_by slug: slug
+  end
 end
