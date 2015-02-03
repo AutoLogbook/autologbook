4f459b528c016e4d938844bc3e3b087d88a03cf6
Tyler Willingham <tylerwillingham@gmail.com>
2015-02-02 22:24:44 -0600

    Vehicle belongs_to User. User has_many Vehicles

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ActiveRecord::Base
   has_many :maintenance_items
+  belongs_to :owner, class_name: 'User',
+                     foreign_key: 'user_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203041742) do
+ActiveRecord::Schema.define(version: 20150203042211) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -93,6 +93,9 @@ ActiveRecord::Schema.define(version: 20150203041742) do
     t.string   "bodystyle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "vehicles", ["user_id"], name: "index_vehicles_on_user_id"
 
 end
